906fe38da286013f36a68fa6640c35e426ca7ac1
Luke Simonetti <lukejsimonetti@gmail.com>
2019-02-03 23:03:43 -0500

    Adding production build

---
+++ build/precache-manifest.df088ef95782ee55b62a9b77670d8662.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e56020ed45eb3805eb8",
+    "url": "/static/js/main.0e56020e.chunk.js"
+  },
+  {
+    "revision": "e1dc24a11902374229b2",
+    "url": "/static/js/1.e1dc24a1.chunk.js"
+  },
+  {
+    "revision": "3be8a4e78c63b71de92c875730d3dcaf",
+    "url": "/index.html"
+  }
+];
