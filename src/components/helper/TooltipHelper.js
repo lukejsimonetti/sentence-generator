b4fc04222906f4609d3e271c2b89647013a88f12
Luke Simonetti <lukejsimonetti@gmail.com>
2019-02-03 22:35:24 -0500

    Adding tooltip helper

---
+++ src/components/helper/TooltipHelper.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import { OverlayTrigger, Tooltip } from 'react-bootstrap'
+
+export const TooltipHelper = ({children, placement, text}) => {
+    return (
+        <OverlayTrigger
+            placement={placement}
+            overlay={
+                <Tooltip id={text}>{text}</Tooltip>
+            }
+        >
+        {children}
+        </OverlayTrigger>
+    )
+}
+
+TooltipHelper.propTypes = {
+    children: PropTypes.object,
+    placement: PropTypes.string,
+    text: PropTypes.string
+}
